e0a51a04115a22391bb781e76659214715b857f6
ISE-EXPLORELOGY <pasan@explorelogy.lk>
2023-10-04 16:33:54 +0530

    feat:login-logout

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,12 +1,19 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { UserContext } from "../UserContext";
 import { Link, Navigate, useParams } from "react-router-dom";
+import axios from "axios";
 
 export default function AccountPage() {
+  const [redirect, setRedirect] = useState(null);
   const { ready, user } = useContext(UserContext);
   let { subpage } = useParams();
   if (subpage === undefined) {
     subpage = "profile";
+  }
+
+  async function logout() {
+    await axios.post("/logout");
+    setRedirect("/");
   }
 
   if (!ready) {
@@ -25,6 +32,10 @@ export default function AccountPage() {
     return classes;
   }
 
+  if (redirect) {
+    return <Navigate to={redirect} />;
+  }
+
   return (
     <div>
       <nav className="w-full flex justify-center mt-8 gap-4">
@@ -38,6 +49,14 @@ export default function AccountPage() {
           My accommadation
         </Link>
       </nav>
+      {subpage === "profile" && (
+        <div className="text-center max-m-lg mx-auto">
+          Logged in as {user.name} ({user.email})<br />
+          <button onClick={logout} className="primary max-w-sm mt-2">
+            Logout
+          </button>
+        </div>
+      )}
     </div>
   );
 }
