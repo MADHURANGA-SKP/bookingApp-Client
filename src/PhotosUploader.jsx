c91b8239c7615cf408f36e8a94ebb4149afad152
Pasan Madhuranga <121794032+ISE-EXPLORELOGY@users.noreply.github.com>
2023-10-29 01:13:25 +0530

    feat:places-page-with-updated

---
+++ src/PhotosUploader.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function PhotosUploader({ addedPhotos, onChange }) {
+  const [photoLink, setPhotoLink] = useState("");
+
+  async function addPhotoByLink(ev) {
+    ev.preventDefault();
+    const { data: filename } = await axios.post("/upload-by-link", {
+      link: photoLink,
+    });
+    onChange((prev) => {
+      return [...prev, filename];
+    });
+    setPhotoLink(" ");
+  }
+  function uploadPhoto(ev) {
+    const files = ev.target.files;
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append("photos", files[i]);
+    }
+    axios
+      .post("/upload", data, {
+        headers: { "Content-type": "multipart/form-data" },
+      })
+      .then((response) => {
+        const { data: filenames } = response;
+        onChange((prev) => {
+          return [...prev, ...filenames];
+        });
+      });
+  }
+  return (
+    <>
+      <div className="flex gap-2">
+        <input
+          type="text"
+          className=""
+          placeholder={"Add using a link ...    JPGE PNG"}
+          value={photoLink}
+          onChange={(ev) => setPhotoLink(ev.target.value)}
+        />
+        <button
+          onClick={addPhotoByLink}
+          className="bg-gray-200 px-4 rounded-2xl"
+        >
+          Add&nbsp;Photos
+        </button>
+      </div>
+
+      <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg-grid-cols-6">
+        {addedPhotos.length > 0 &&
+          addedPhotos.map((link) => (
+            <div className="h-32 flex" key={link}>
+              {console.log(
+                "Image Path:",
+                "http://localhost:4000/uploads/" + link
+              )}
+              <img
+                className="rounded-2xl"
+                src={"http://localhost:4000/uploads/" + link}
+                alt=""
+              />
+            </div>
+          ))}
+        <label className="h-32 flex justify-center cursor-pointer gap-2 border bg-transparent rounded-2xl items-center p-2 text-2xl text-gray-600">
+          <input
+            type="file"
+            className="hidden"
+            onChange={uploadPhoto}
+            multiple={true}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-8 h-8"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
+            />
+          </svg>
+          Upload
+        </label>
+      </div>
+    </>
+  );
+}
